ff7c05df94d6403b8db85a2d075171ab5b09a917
Kpraveen07 <Kpraveenk701@gmail.com>
2022-11-17 23:52:59 +0530

    done

---
+++ Even_Odd.java
@@ -0,0 +1,16 @@
+
+//To check if number is even or odd
+
+public class EvenOdd 
+{
+
+	public static void main(String[] args) 
+	{
+		int x=10;
+		if (x==10)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
